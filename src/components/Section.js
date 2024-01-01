9931d705a7903576da76d71b813bf598586c943c
Athinamilagi <kvigneshvicky6794@gmail.com>
2024-01-01 12:18:56 +0530

    Added Sections for Tesla clone

---
+++ src/components/Section.js
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+import carDetail from "../carInfo.json";
+function Section() {
+  const itemDetails = carDetail.map((data) => (
+    <Wrap key={data.id} bgimage={data.image}>
+      <ItemText>
+        <h1>{data.title}</h1>
+        <p>{data.description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" alt="down-arrow" />
+      </Buttons>
+    </Wrap>
+  ));
+
+  return <>{itemDetails}</>;
+}
+
+export default Section;
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-image: ${props => `url("/images/${props.bgimage}")`};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ItemText = styled.div`
+  padding: 15vh;
+  text-align: center;
+`;
+const Buttons = styled.div``;
+const ButtonGroup = styled.div`
+  display: flex;
+  margin: 40px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+const LeftButton = styled.div`
+  display: flex;
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
+  text-transform: uppercase;
+  color: white;
+  border-radius: 100px;
+  opacity: 0.8;
+  cursor: pointer;
+  margin: 8px;
+`;
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  opacity: 0.6;
+  color: black;
+`;
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
